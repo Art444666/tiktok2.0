01b2805ef16f5c9f20302360f8f5d2b7d6bd5aa6
Art444666 <goritskijartem0@gmail.com>
2025-11-28 22:01:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, redirect, url_for, session, render_template_string
+import os
 
 app = Flask(__name__)
 app.config["SESSION_TYPE"] = "filesystem"
@@ -168,4 +169,6 @@ def delete_comment(index):
     return redirect(url_for("admin_console"))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render задаёт PORT автоматически
+    app.run(host="0.0.0.0", port=port)
+
